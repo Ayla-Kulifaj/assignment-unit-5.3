1a267aa996d9f9dc15b53ed1485a56f2eb2e7e84
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-03-18 22:51:07 -0600

    Object & functions to gather information of 6 different albums.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,21 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+myCollection=[];
 
-
-
-
-
-
-
+function addToCollection(collection, title, artist, yearPublished) {
+  const album={title, artist, yearPublished };
+  collection.push(album);
+  return album;
+}
+console.log("Adding albums to myCollection: ");
+const albumsToAdd=[
+{ title: "Back to Black", artist: "Amy Winehouse", yearPublished: 2006 },
+{ title: "Abbey Road", artist: "The Beatles", yearPublished: 1969 },
+{ title: "Expectations", artist: "Wild Child", yearPublished: 2018 },
+{ title: "Hozier", artist: "Hozier", yearPublished: 2014 },
+{ title: "Tracy Chapman", artist: "Tracy Chapman", yearPublished: 1988 },
+{ title: "There's Really A Wolf", artist: "Russ", yearPublished: 2017 }
+];
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
